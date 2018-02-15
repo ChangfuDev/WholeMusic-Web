986f5b8593c451adc59114115d10ff600a1be883
eggfly <eggfly@qq.com>
2018-02-16 02:53:57 +0800

    remove getUser
    Signed-off-by: eggfly <eggfly@qq.com>

--- webapp/src/main/java/wholemusic/web/controller/OAuthCallbackController.java
+++ webapp/src/main/java/wholemusic/web/controller/OAuthCallbackController.java
@@ -4,6 +4,7 @@ import com.belerweb.social.bean.Result;
 import com.belerweb.social.weibo.api.User;
 import com.belerweb.social.weibo.api.Weibo;
 import com.belerweb.social.weibo.bean.AccessToken;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,7 @@ import wholemusic.web.util.AccountHelper;
 /**
  * Created by haohua on 2018/2/15.
  */
-@RestController
+@Controller
 @RequestMapping("/oauth/callback")
 @SuppressWarnings("unused")
 public class OAuthCallbackController extends ControllerWithSession {
@@ -21,7 +22,6 @@ public class OAuthCallbackController extends ControllerWithSession {
     @GetMapping("/weibo")
     public String weibo(@RequestParam("code") String code) {
         Weibo weibo = AccountHelper.getWeibo();
-        User userApi = weibo.getUser();
         Result<AccessToken> tokenResult = weibo.getOAuth2().accessToken(code);
         final boolean tokenSuccess = tokenResult.success();
         if (tokenSuccess) {
