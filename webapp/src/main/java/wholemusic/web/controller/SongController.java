55d254dd172643884e284d061e28e4380049a75c
eggfly <eggfly@qq.com>
2018-02-17 01:14:27 +0800

    song search controller
    Signed-off-by: eggfly <eggfly@qq.com>

--- webapp/src/main/java/wholemusic/web/controller/SongController.java
+++ webapp/src/main/java/wholemusic/web/controller/SongController.java
@@ -1,30 +1,30 @@
 package wholemusic.web.controller;
 
-import org.springframework.boot.ApplicationHome;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import wholemusic.core.api.MusicApi;
+import wholemusic.core.api.MusicApiFactory;
+import wholemusic.core.api.MusicProvider;
+import wholemusic.core.model.Song;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Paths;
+import java.util.List;
 
 /**
  * Created by haohua on 2018/2/13.
  */
-@RestController
+@Controller
 @RequestMapping("/song")
 @SuppressWarnings("unused")
 public class SongController {
-
-    @GetMapping("/")
-    public String index() {
-        return "song index :";
-    }
-
-    @GetMapping("/search/{query}")
-    public String search(String query) {
-        return "song search: " + query;
+    @GetMapping("/search")
+    public String search(@RequestParam("query") String query, ModelMap map) throws IOException {
+        MusicApi api = MusicApiFactory.create(MusicProvider.Neteast);
+        List<? extends Song> result = api.searchMusicSync(query, 0);
+        map.addAttribute("songs", result);
+        return "song/search";
     }
 }
