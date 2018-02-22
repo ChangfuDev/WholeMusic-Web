35612306bfc0deb570cdc0d6a9f1684c1ffac066
eggfly <eggfly@qq.com>
2018-02-22 18:32:42 +0800

    download zip
    Signed-off-by: eggfly <eggfly@qq.com>

--- webapp/src/main/java/wholemusic/web/controller/AdminDiskController.java
+++ webapp/src/main/java/wholemusic/web/controller/AdminDiskController.java
@@ -4,7 +4,6 @@ package wholemusic.web.controller;
  * Created by haohua on 2018/2/14.
  */
 
-import com.sun.media.jfxmediaimpl.MediaUtils;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,17 +13,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import wholemusic.core.util.TextUtils;
 import wholemusic.web.model.domain.User;
 import wholemusic.web.util.FileUtils;
 import wholemusic.web.util.WeiboAccountHelper;
+import wholemusic.web.util.ZipUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLDecoder;
+import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 
 @Controller
@@ -40,13 +41,10 @@ public class AdminDiskController extends ControllerWithSession {
     }
 
     @GetMapping({"/", "/**"})
-    public Object index(HttpServletRequest request, ModelMap map) throws MalformedURLException,
-            UnsupportedEncodingException {
+    public Object index(HttpServletRequest request, ModelMap map) throws IOException {
         User user = getCurrentUser();
         if (WeiboAccountHelper.isAdminUser(user)) {
-            URL url = new URL(request.getRequestURL().toString());
-            String relativePath = URLDecoder.decode(
-                    url.getPath().replaceFirst(PATH_PREFIX + "/", ""), StandardCharsets.UTF_8.name());
+            String relativePath = getRelativePathFromRequest(request);
             File target = getFile(relativePath);
             if (!target.exists()) {
                 throw new ResourceNotFoundException();
@@ -64,8 +62,9 @@ public class AdminDiskController extends ControllerWithSession {
                 } else {
                     // 文件
                     HttpHeaders headers = new HttpHeaders();
-                    // TODO: dynamic file type
-                    headers.setContentType(MediaType.valueOf(MediaUtils.CONTENT_TYPE_MPA));
+                    MediaType type = guessMimeType(target);
+                    headers.setContentType(type);
+                    headers.setContentDispositionFormData("attachment", target.getName(), StandardCharsets.UTF_8);
                     FileSystemResource fileSystemResource = new FileSystemResource(target);
                     return new ResponseEntity<>(fileSystemResource, headers, HttpStatus.OK);
                 }
@@ -73,6 +72,25 @@ public class AdminDiskController extends ControllerWithSession {
         } else {
             return "redirect:/";
         }
+    }
+
+    private static MediaType guessMimeType(File target) throws IOException {
+        String mime = Files.probeContentType(target.toPath());
+        if (mime == null) {
+            mime = URLConnection.guessContentTypeFromName(target.getAbsolutePath());
+        }
+        if (mime != null) {
+            return MediaType.valueOf(mime);
+        } else {
+            return MediaType.APPLICATION_OCTET_STREAM;
+        }
+    }
+
+    private static String getRelativePathFromRequest(HttpServletRequest request) throws MalformedURLException,
+            UnsupportedEncodingException {
+        URL url = new URL(request.getRequestURL().toString());
+        return URLDecoder.decode(
+                url.getPath().replaceFirst(PATH_PREFIX + "/", ""), StandardCharsets.UTF_8.name());
     }
 
     private static class RelativeFile {
@@ -99,5 +117,34 @@ public class AdminDiskController extends ControllerWithSession {
         File downloadDir = FileUtils.getDownloadDir(false);
         File target = new File(downloadDir, relativePath);
         return target;
+    }
+
+    @GetMapping("/zip/**")
+    public String prepareZip(HttpServletRequest request) throws MalformedURLException, UnsupportedEncodingException {
+        User user = getCurrentUser();
+        if (WeiboAccountHelper.isAdminUser(user)) {
+            String relativePath = getRelativePathFromRequest(request);
+            relativePath = relativePath.replaceFirst("zip/", "");
+            final File source = new File(FileUtils.getDownloadDir(true), relativePath);
+            if (source.exists()) {
+                File zip = ZipUtil.zip(source.getAbsolutePath());
+                String redirectPath = PATH_PREFIX + "/" + relativePath.substring(0, relativePath.length() - 1) + ".zip";
+                redirectPath = urlEncodePath(redirectPath);
+                return "redirect:" + redirectPath;
+            } else {
+                throw new ResourceNotFoundException();
+            }
+        } else {
+            return "redirect:/";
+        }
+    }
+
+    private static String urlEncodePath(String path) throws UnsupportedEncodingException {
+        String[] parts = path.split("/");
+        ArrayList<String> list = new ArrayList<>();
+        for (String part : parts) {
+            list.add(URLEncoder.encode(part, StandardCharsets.UTF_8.name()));
+        }
+        return TextUtils.join("/", list);
     }
 }
